1fe68064a138fb45938dbfe06b4f4002df060341
Antonio Vuono <antoniosvuono@icloud.com>
2024-04-11 20:52:20 -0300

    chore: fastify and tsx installed, hello world started

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import fastify from "fastify"
+
+const app = fastify()
+
+
+app.get("/", () => {
+    return { hello: "world" }
+})
+
+app.listen({host: '0.0.0.0', port: 3333}).then(() => console.log("🚀 Server is running on port 3333"))
