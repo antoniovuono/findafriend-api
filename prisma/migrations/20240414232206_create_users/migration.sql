66641bc49a36a4149d9096863c62e230987a82db
Antonio Vuono <antoniosvuono@icloud.com>
2024-04-14 20:24:44 -0300

    feature: create-user migration created

---
+++ prisma/migrations/20240414232206_create_users/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "UserType" AS ENUM ('ORGANIZATION', 'NORMAL');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "whatsapp" TEXT NOT NULL,
+    "user_type" "UserType" NOT NULL DEFAULT 'NORMAL',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_whatsapp_key" ON "users"("whatsapp");
