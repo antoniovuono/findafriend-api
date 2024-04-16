650f120f4140800b99ff13744235cff3aa4e3e85
Antonio Vuono <antoniosvuono@icloud.com>
2024-04-16 15:41:32 -0300

    test: unity tests from create-users service created

---
+++ src/services/create-user.test.ts
@@ -0,0 +1,68 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { CreateUserService } from './create-user'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { compare } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+let usersRepository: InMemoryUsersRepository
+let createUserService: CreateUserService
+
+describe('Create User Service', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    createUserService = new CreateUserService(usersRepository)
+  })
+
+  it('should be able to create a normal user', async () => {
+    const { user } = await createUserService.execute({
+      name: 'John Doe ',
+      email: 'jhondoe@example.com',
+      password: '123456',
+      whatsapp: '123456789',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should be able to create an organization user', async () => {
+    const { user } = await createUserService.execute({
+      name: 'John Doe ',
+      email: 'jhondoe@example.com',
+      password: '123456',
+      whatsapp: '123456789',
+      userType: 'ORGANIZATION',
+    })
+
+    expect(user.user_type).toEqual('ORGANIZATION')
+  })
+
+  it('should password must be hashed', async () => {
+    const { user } = await createUserService.execute({
+      name: 'John Doe ',
+      email: 'jhondoe@example.com',
+      password: '123456',
+      whatsapp: '123456789',
+    })
+
+    const isPasswordHashed = await compare('123456', user.password)
+
+    expect(isPasswordHashed).toBeTruthy()
+  })
+  it('should not be able to create a user with an existing email', async () => {
+    await createUserService.execute({
+      name: 'John Doe ',
+      email: 'jhondoe@example.com',
+      password: '123456',
+      whatsapp: '123456789',
+    })
+
+    await expect(async () => {
+      await createUserService.execute({
+        name: 'John Doe ',
+        email: 'jhondoe@example.com',
+        password: '123456',
+        whatsapp: '123456789',
+      })
+    }).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+})
