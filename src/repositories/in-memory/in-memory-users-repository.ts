4bd6e7a6a1347ccb731375109bd4957efad9629a
Antonio Vuono <antoniosvuono@icloud.com>
2024-04-16 15:33:06 -0300

    feature: in-memory-users-repository and users interface created

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,36 @@
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public users: User[] = []
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      whatsapp: data.whatsapp,
+      user_type: data.user_type ?? null,
+      address: data.address ?? null,
+      city: data.city ?? null,
+      postal_code: data.postal_code ?? null,
+      created_at: new Date(),
+    }
+
+    this.users.push(user)
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = this.users.find((user) => user.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+}
