a7ec90e17966f279784834f316a165bb4d3d3467
Antonio Vuono <antoniosvuono@icloud.com>
2024-04-16 15:41:15 -0300

    feature: create-users service and personalized error created

---
+++ src/services/create-user.ts
@@ -0,0 +1,57 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+type UserTypeProps = 'ORGANIZATION' | 'NORMAL'
+
+interface CreateUserServiceRequest {
+  name: string
+  email: string
+  password: string
+  whatsapp: string
+  userType?: UserTypeProps
+  address?: string
+  city?: string
+  postalCode?: string
+}
+
+interface CreateUserServiceResponse {
+  user: User
+}
+
+export class CreateUserService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    name,
+    email,
+    password,
+    whatsapp,
+    userType,
+    address,
+    city,
+    postalCode,
+  }: CreateUserServiceRequest): Promise<CreateUserServiceResponse> {
+    const passwordHash = await hash(password, 8)
+
+    const emailAlreadyRegistered = await this.usersRepository.findByEmail(email)
+
+    if (emailAlreadyRegistered) {
+      throw new UserAlreadyExistsError()
+    }
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: passwordHash,
+      whatsapp,
+      user_type: userType,
+      address,
+      city,
+      postal_code: postalCode,
+    })
+
+    return { user }
+  }
+}
