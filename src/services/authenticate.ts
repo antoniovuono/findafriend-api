cd41d6bd39688c69164a28e4614c7aa3542dd76d
Antonio Vuono <antoniosvuono@icloud.com>
2024-04-18 15:39:28 -0300

    feature: authenticate service created

---
+++ src/services/authenticate.ts
@@ -0,0 +1,36 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { compare } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+interface AuthenticateServiceRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateServiceResposnse {
+  user: User
+}
+
+export class AuthenticateService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateServiceRequest): Promise<AuthenticateServiceResposnse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatch = await compare(password, user.password)
+
+    if (!doesPasswordMatch) {
+      throw new InvalidCredentialsError()
+    }
+
+    return { user }
+  }
+}
