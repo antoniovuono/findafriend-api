398c1522a70f537bb3d507ad0d1fa3304fe14821
Antonio Vuono <antoniosvuono@icloud.com>
2024-04-18 20:37:07 -0300

    feature: pets repository created

---
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -0,0 +1,40 @@
+import { Pet, Prisma } from '@prisma/client'
+import { PetsRepository } from '../pets-repository'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryPetsRepository implements PetsRepository {
+  public pet: Pet[] = []
+
+  async create(data: Prisma.PetUncheckedCreateInput): Promise<Pet> {
+    const pet = {
+      id: randomUUID(),
+      surname: data.surname,
+      color: data.color,
+      city: data.city,
+      age: data.age,
+      description: data.description,
+      user_id: data.user_id,
+      created_at: new Date(),
+    }
+
+    this.pet.push(pet)
+
+    return pet
+  }
+
+  async findById(id: string): Promise<Pet | null> {
+    const pet = this.pet.find((pet) => pet.id === id)
+
+    if (!pet) {
+      return null
+    }
+
+    return pet
+  }
+
+  async findManyByCity(city: string) {
+    const pets = this.pet.filter((pet) => pet.city === city)
+
+    return pets
+  }
+}
