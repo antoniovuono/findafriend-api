0787c6e4500360c0a0dd51fb5829d42ee75411d0
Antonio Vuono <antoniosvuono@icloud.com>
2024-04-18 21:52:24 -0300

    test: create-org-user rule tests created

--- src/services/create-user.test.ts
+++ src/services/create-user.test.ts
@@ -3,6 +3,7 @@ import { CreateUserService } from './create-user'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { compare } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { IncompleteInputError } from './errors/incomplete-input-error'
 
 let usersRepository: InMemoryUsersRepository
 let createUserService: CreateUserService
@@ -31,6 +32,9 @@ describe('Create User Service', () => {
       password: '123456',
       whatsapp: '123456789',
       userType: 'ORGANIZATION',
+      address: 'Rua Example',
+      city: 'Example City',
+      postalCode: '123456',
     })
 
     expect(user.user_type).toEqual('ORGANIZATION')
@@ -64,5 +68,17 @@ describe('Create User Service', () => {
         whatsapp: '123456789',
       })
     }).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+
+  it('should not be able to create an organization user without address, city and postal code', async () => {
+    await expect(async () => {
+      await createUserService.execute({
+        name: 'John Doe ',
+        email: 'jhondoe@example.com',
+        password: '123456',
+        whatsapp: '123456789',
+        userType: 'ORGANIZATION',
+      })
+    }).rejects.toBeInstanceOf(IncompleteInputError)
   })
 })
