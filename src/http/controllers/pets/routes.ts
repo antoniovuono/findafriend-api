38aacdc431d5f7e7d0052cef5254b594a6488551
Antonio Vuono <antoniosvuono@icloud.com>
2024-04-25 19:39:10 -0300

    feature: petRotues created

---
+++ src/http/controllers/pets/routes.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from 'fastify'
+import { create } from './create'
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { verifyUserType } from '@/http/middlewares/verify-user-type'
+
+export async function petsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJwt)
+
+  app.post('/pets', { onRequest: [verifyUserType('ORGANIZATION')] }, create)
+}
