f0be6dc8ac6f909981b3ac13a00b419a74683f30
Antonio Vuono <antoniosvuono@icloud.com>
2024-04-25 14:43:53 -0300

    feature: fastify-jwt installed and configured

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,16 @@ import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
 import { usersRoutes } from './http/controllers/users/routes'
+import fastifyJwt from '@fastify/jwt'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: '7d',
+  },
+})
 
 app.register(usersRoutes)
 
